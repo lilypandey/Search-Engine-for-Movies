aa53a858f904d2015ff9307f103866a4b69818bb
lily-notaflower <panlily2002@gmail.com>
2025-01-09 21:51:42 +0530

    Update app.py
    Adding .strip().lower() in case of extra spaces

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ def home():
 
 @app.route('/search', methods = ['GET'])
 def search():
-    query = request.args.get('query','').lower()
+    query = request.args.get('query','').strip().lower()
     movie_manager = MovieManager()
     results = movie_manager.cached_data(query)
 
     return render_template('home.html', movies=results)
 
 if __name__ == "__main__":
-    app.run(debug="True")
+    app.run(debug=True)
