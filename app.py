4b7394ec6371a2e8f1933440761939f0cf5f3231
lilypandey <panlily2002@gmail.com>
2025-03-04 11:09:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from script import MovieManager
 
-app=Flask(__name__)
+app = Flask(__name__)
+movie_manager = MovieManager()
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/search', methods = ['GET'])
+@app.route('/search', methods=['GET'])
 def search():
-    query = request.args.get('query','').strip().lower()
-    movie_manager = MovieManager()
+    query = request.args.get('query', '').strip().lower()
     results = movie_manager.cached_data(query)
-
     return render_template('home.html', movies=results)
+
+@app.route('/top250', methods=['GET'])
+def top_250():
+    top_movies = movie_manager.fetch_imdb_top_250()
+    return render_template('home.html', top_movies=top_movies)
 
 if __name__ == "__main__":
     app.run(debug=True)
